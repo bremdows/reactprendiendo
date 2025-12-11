5692435644b9a9345a528a05bf877d05e4419a44
dbsalazarr <dbsalazarr@gmail.com>
2025-12-11 12:03:18 -0500

    Primer componente

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,24 +1,11 @@
-import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
     return (
-        <article 
-            style={{display:'flex', alignItems:'center'}}
-            className='br-followCard'
-        >
-            <header className='br-followCard-header'>
-                <img src="https://unavatar.io/x/bremdows" alt="Logo marca" className='br-followCard-avatar'/>
-                <div className='br-followCard-info'>
-                    <strong className='br-followCard-infoUserName'>Bremdow Salazar Roa</strong>
-                    <span>@bremdows_dev</span>
-                </div>
-            </header>
-
-            <aside>
-                <button className='br-followCard-button'>
-                    Seguir
-                </button>
-            </aside>
-        </article>
+        <section className='app'>
+            <TwitterFollowCard userName="bremdows_dev" name="Davis Bremdow Salazar Roa" isFollowing={true}/>
+            <TwitterFollowCard userName="ruth_juana" name="Ruth Espino Puma" isFollowing={false}/>
+            <TwitterFollowCard userName="bremdows_dev" name="Davis Bremdow Salazar Roa" isFollowing={true}/>
+        </section>
     )
 }
---
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+export function TwitterFollowCard({userName, name, isFollowing}){
+    return(
+        <article 
+            style={{display:'flex', alignItems:'center'}}
+            className='br-followCard'
+        >
+            <header className='br-followCard-header'>
+                <img src={`https://unavatar.io/x/${userName}`} alt="Logo marca" className='br-followCard-avatar'/>
+                <div className='br-followCard-info'>
+                    <strong className='br-followCard-infoUserName'>{name}</strong>
+                    <span>@{userName}</span>
+                </div>
+            </header>
+
+            <aside>
+                <button className='br-followCard-button'>
+                    Seguir
+                </button>
+            </aside>
+        </article>
+    )
+}
