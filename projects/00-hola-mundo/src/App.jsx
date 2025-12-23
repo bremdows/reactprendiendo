c91e894e694907f4bf109c32f7e3faed2d2486c7
dbsalazarr <dbsalazarr@gmail.com>
2025-12-22 22:22:12 -0500

    Propiedades y funciones como parámetros

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,11 +1,30 @@
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
+    const formatUserName = (userName) => `@${userName}`
     return (
         <section className='app'>
-            <TwitterFollowCard userName="bremdows_dev" name="Davis Bremdow Salazar Roa" isFollowing={true}/>
-            <TwitterFollowCard userName="ruth_juana" name="Ruth Espino Puma" isFollowing={false}/>
-            <TwitterFollowCard userName="bremdows_dev" name="Davis Bremdow Salazar Roa" isFollowing={true}/>
+            <TwitterFollowCard 
+             formatUserName={formatUserName}
+             userName="bremdows_dev" 
+             name="Davis Bremdow Salazar Roa" 
+             isFollowing={true}/>
+
+            <TwitterFollowCard 
+             formatUserName={formatUserName}
+             userName="x" name="Ruth Espino Puma" 
+             isFollowing={false}/>
+
+            <TwitterFollowCard 
+             formatUserName={formatUserName}
+             userName="bremdows_dev" 
+             name="Davis Bremdow Salazar Roa" 
+             isFollowing/>
+
+            <TwitterFollowCard 
+             formatUserName={formatUserName}
+             userName="youtube"
+             name="Youtube" />
         </section>
     )
 }
