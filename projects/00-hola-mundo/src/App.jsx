6eea02bd4798faaf7bff1d57aae3263058156869
dbsalazarr <dbsalazarr@gmail.com>
2026-01-01 23:05:52 -0500

    useState react

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,30 +1,41 @@
+import { useState } from 'react'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
     const formatUserName = (userName) => `@${userName}`
+
+    const [name, setName] = useState("MoureDev")
+    
+    const [isFollowing, setIsFollowing] = useState(false)
+
+    console.log("[App] Actualizado desde: " + isFollowing)
+
     return (
         <section className='app'>
             <TwitterFollowCard 
+             userName={name}
              formatUserName={formatUserName}
-             userName="bremdows_dev" 
-             name="Davis Bremdow Salazar Roa" 
-             isFollowing={true}/>
+             name="Davis Bremdow Salazar Roa"
+             initialIsFollowing={isFollowing}
+             />
 
             <TwitterFollowCard 
              formatUserName={formatUserName}
-             userName="x" name="Ruth Espino Puma" 
-             isFollowing={false}/>
+             userName="x"
+             name="X Twitter"
+             initialIsFollowing={isFollowing}
+             />
 
-            <TwitterFollowCard 
-             formatUserName={formatUserName}
-             userName="bremdows_dev" 
-             name="Davis Bremdow Salazar Roa" 
-             isFollowing/>
+             <TwitterFollowCard
+                userName="claroperu"
+                name="Claro E.I.R.L"
+                formatUserName={formatUserName}
+                initialIsFollowing={isFollowing}
+                >
+             </TwitterFollowCard>
 
-            <TwitterFollowCard 
-             formatUserName={formatUserName}
-             userName="youtube"
-             name="Youtube" />
+             <button onClick={ () => { setName("freddier") }}> Cambiar estado </button>
+             <button onClick={() => {setIsFollowing(!isFollowing) }}> Cambiar estado 2</button>
         </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,6 +1,23 @@
+import { useState } from 'react'
+
 import './App.css'
-export function TwitterFollowCard({userName, name, isFollowing, formatUserName}){
-    console.log(isFollowing)
+
+export function TwitterFollowCard({children, userName, name, formatUserName, initialIsFollowing}){
+
+    const [isFollowing, setIsFollowing] = useState(initialIsFollowing)
+
+    const textButton = isFollowing ? 'Siguiendo' : 'Seguir'
+    const classButton = isFollowing
+    ? 'br-followCard-button is-following'
+    : 'br-followCard-button'
+
+    console.log("[Comp] Actualizado desde: " + userName)
+
+
+    const handleClick = () => {
+        setIsFollowing(!isFollowing)
+    }
+
     return(
         <article 
             // style={{display:'flex', alignItems:'center'}}
@@ -10,13 +27,13 @@ export function TwitterFollowCard({userName, name, isFollowing, formatUserName})
                 <img src={`https://unavatar.io/x/${userName}`} alt="Logo marca" className='br-followCard-avatar'/>
                 <div className='br-followCard-info'>
                     <strong className='br-followCard-infoUserName'>{name}</strong>
-                    <span>{formatUserName}</span>
+                    <span>{formatUserName(userName)}</span>
+                    <p>{children}</p>
                 </div>
             </header>
-            
             <aside>
-                <button className='br-followCard-button'>
-                    Seguir
+                <button className={classButton} onClick={handleClick}>
+                     {textButton}
                 </button>
             </aside>
         </article>
