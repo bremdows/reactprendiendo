52c81079de0449f5b482ede1018e005daab96c40
dbsalazarr <dbsalazarr@gmail.com>
2026-01-15 20:48:00 -0500

    Proyecto de repaso finalizado

--- projects/00-learning-components/src/App.jsx
+++ projects/00-learning-components/src/App.jsx
@@ -4,54 +4,108 @@ import { CardSell } from './CardSell'
 function App() {
 
   const setEventPrice = (price) => `$ ${price}`
+  
+  const artists = [
+    {
+      name: "Dua Lipa",
+      idImage : "https://media.vogue.mx/photos/5c07028beccb767eb579ef95/2:3/w_2560%2Cc_limit/dua_lipa__7600.jpg",
+      category:"Music",
+      price:2500,
+      language: "English",
+      isLiked: true
+    },
+    {
+      name: "Shakira",
+      idImage : "https://cdn-images.dzcdn.net/images/artist/69c569506a8ff6ab0edfecbd1adf94b0/1900x1900-000000-80-0-0.jpg",
+      category:"Music",
+      price:1500,
+      language: "Spanish",
+      isLiked: true
+    },
+    {
+      name:"Selena Gomez",
+      idImage : "https://m.media-amazon.com/images/M/MV5BMjAwNTE2MDMyMl5BMl5BanBnXkFtZTgwMjAyODM3MTI@._V1_FMjpg_UX1000_.jpg",
+      category:"Music",
+      price:2000,
+      language: "English",
+      isLiked: true
+    },
+    {
+      name: "Ana de Armas",
+      idImage : "https://hips.hearstapps.com/hmg-prod/images/ana-de-armas-6841b672bced5.jpg",
+      category:"Movies",
+      price:20,
+      language: "English",
+      isLiked: false
+    }
+  ]
 
   return (
     <section className="main-cards">
-      <CardSell
-        idImage="https://media.vogue.mx/photos/5c07028beccb767eb579ef95/2:3/w_2560%2Cc_limit/dua_lipa__7600.jpg"
-        category="Music"
-        price={2500}
-        formatPrice={setEventPrice}
-        language="English"
-        isLiked
-      >
-          Dua Lipa
-      </CardSell>
-
-      <CardSell
-        idImage="https://cdn-images.dzcdn.net/images/artist/69c569506a8ff6ab0edfecbd1adf94b0/1900x1900-000000-80-0-0.jpg"
-        category="Music"
-        price={1500}
-        formatPrice={setEventPrice}
-        language="Spanish"
-        isLiked={false}
-      >
-        Shakira
-      </CardSell>
-
-      <CardSell
-        idImage="https://m.media-amazon.com/images/M/MV5BMjAwNTE2MDMyMl5BMl5BanBnXkFtZTgwMjAyODM3MTI@._V1_FMjpg_UX1000_.jpg"
-        category="Music"
-        price={2000}
-        formatPrice={setEventPrice}
-        language="English"
-        isLiked
-      >
-        Selena Gomez
-      </CardSell>
-
-      <CardSell
-        idImage="https://hips.hearstapps.com/hmg-prod/images/ana-de-armas-6841b672bced5.jpg"
-        category="Movies"
-        price={20}
-        formatPrice={setEventPrice}
-        language="English"
-        isLiked={false}
-      >
-        Ana de Armas
-      </CardSell>
+      {
+        artists.map( (art) => {
+          return (
+            <CardSell
+              key={art.idImage}
+              idImage={art.idImage}
+              category={art.music}
+              price={art.price}
+              formatPrice={setEventPrice}
+              language={art.language}
+              isLiked={art.isLiked}
+            >
+              {art.name}
+            </CardSell>
+          )
+        })
+      }
     </section>
   )
 }
 
 export default App
+
+
+      // <CardSell
+      //   idImage="https://media.vogue.mx/photos/5c07028beccb767eb579ef95/2:3/w_2560%2Cc_limit/dua_lipa__7600.jpg"
+      //   category="Music"
+      //   price={2500}
+      //   formatPrice={setEventPrice}
+      //   language="English"
+      //   isLiked
+      // >
+      //     Dua Lipa
+      // </CardSell>
+
+      // <CardSell
+      //   idImage="https://cdn-images.dzcdn.net/images/artist/69c569506a8ff6ab0edfecbd1adf94b0/1900x1900-000000-80-0-0.jpg"
+      //   category="Music"
+      //   price={1500}
+      //   formatPrice={setEventPrice}
+      //   language="Spanish"
+      //   isLiked={false}
+      // >
+      //   Shakira
+      // </CardSell>
+
+      // <CardSell
+      //   idImage="https://m.media-amazon.com/images/M/MV5BMjAwNTE2MDMyMl5BMl5BanBnXkFtZTgwMjAyODM3MTI@._V1_FMjpg_UX1000_.jpg"
+      //   category="Music"
+      //   price={2000}
+      //   formatPrice={setEventPrice}
+      //   language="English"
+      //   isLiked
+      // >
+      //   Selena Gomez
+      // </CardSell>
+
+      // <CardSell
+      //   idImage="https://hips.hearstapps.com/hmg-prod/images/ana-de-armas-6841b672bced5.jpg"
+      //   category="Movies"
+      //   price={20}
+      //   formatPrice={setEventPrice}
+      //   language="English"
+      //   isLiked={false}
+      // >
+      //   Ana de Armas
+      // </CardSell>
--- projects/00-learning-components/src/CardSell.jsx
+++ projects/00-learning-components/src/CardSell.jsx
@@ -9,22 +9,29 @@ import './font-style.css'
     const [like, setLike] = useState(isLiked)
     const classIconLike = like ? "icon-heart1" : "icon-heart"
 
+
+    const [name, setName] = useState(children)
+
     const handLike = () => {
         setLike(!like)
+    }
+
+    const changeName = () => {
+        setName(`${name} new`)
     }
 
     return(
         <article className="card-sell">
             <img className="card-image" src={idImage} alt={`Imagen {idImage} no encontrada`} />
             <div className="card-content">
-                <h3 className="card-content-title">{children}</h3>
+                <h3 className="card-content-title">{name}</h3>
                 <span className="card-content-category">{category}</span>
                 <div className="card-content-price">
                     <p> <span className="icon-buy"></span> from <strong>{formatPrice(price)}</strong> </p>
                     <p> <span className="icon-plane"></span> in <strong>{language}</strong> </p>
                 </div>
                 <div className="card-button">
-                    <button className="card-button-booking">Booking Event</button>
+                    <button className="card-button-booking" onClick={changeName}>Booking Event</button>
                     <button className="card-button-like" onClick={handLike}> <span className={classIconLike}></span> </button>
                 </div>
             </div>
