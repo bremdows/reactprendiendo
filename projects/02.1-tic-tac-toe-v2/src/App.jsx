8e43cf195057ae6fd7c34318445e43e48398fe7a
bremdows <bremdows@gmail.com>
2026-05-17 11:07:11 -0500

    creaciòn del tablero

---
+++ projects/02.1-tic-tac-toe-v2/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import './App.css'
+
+
+const TURNS = {
+  X:'x',
+  O: 'o'
+}
+
+
+
+const Square = ({children, updateBoard, index}) => {
+  return (
+    <div className="square">
+      {children}
+    </div>
+  )
+}
+
+function App(){
+
+  const [board, setBoard] =  useState(Array(9).fill(null))
+  console.log(board)
+  return (
+    <main className="board">
+      <h1>Tic tac toe</h1>
+      <section className="game">
+       {
+         board.map( (_, index) => {
+          return (
+            <Square 
+              key={index}
+              index={index}
+            >
+              {index}
+            </Square>
+          )
+        })
+       }
+      </section>
+    </main>
+  )
+}
+
+export default App
